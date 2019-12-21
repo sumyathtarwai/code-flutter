a5621552c87ce1517a4e3cc39363341bed330753
sumyathtarwai <hsumyathtarwai@gmail.com>
2019-12-21 11:06:21 +0630

    init

---
+++ flutter/wiget_state/lib/ui/my_home.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MyHome extends StatefulWidget {
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  var _count = 0;
+  void increment() {
+    setState(() {
+      _count++;
+    });
+  }
+
+  void decrement() {
+    setState(() {
+      _count--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Hello Widget State')),
+      body: Center(child: Text("Hello Flutter $_count")),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(onPressed: increment, child: Icon(Icons.add)),
+          FloatingActionButton(onPressed: decrement, child: Icon(Icons.remove)),
+        ],
+      ),
+    );
+  }
+}
