e6779dcd9d3ca266f9458e2a128399ce59d27ee8
sumyathtarwai <hsumyathtarwai@gmail.com>
2020-01-11 11:01:46 +0630

    added another page with flexibleSpace

--- flutter/ui_building/lib/main.dart
+++ flutter/ui_building/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'ui/home_page.dart';
+import 'package:ui_building/ui/another_home.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.orangeAccent),
-      home: MyHome(),
+      home: AnotherHome(),
     );
   }
 }
