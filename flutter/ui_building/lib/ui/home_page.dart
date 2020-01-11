0442043413c81ecd5af541554bc8a9df6da8b974
sumyathtarwai <hsumyathtarwai@gmail.com>
2020-01-11 10:28:57 +0630

    added home page

--- flutter/ui_building/lib/ui/home_page.dart
+++ flutter/ui_building/lib/ui/home_page.dart
@@ -7,8 +7,45 @@ class MyHome extends StatelessWidget {
       appBar: AppBar(
         title: Text('MyHome'),
       ),
-      body: Center(
-        child: CircularProgressIndicator(),
+      body: Container(
+        width: 300.0,
+        height: 300.0,
+        color: Colors.black,
+        child: Stack(
+          children: <Widget>[
+            buildCard(
+              Colors.green,
+              100.0,
+              100.0,
+            ),
+            buildCard(
+              Colors.yellow,
+              50.0,
+              50.0,
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: buildCard(
+                Colors.cyan,
+                50.0,
+                50.0,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Card buildCard(Color color, double width, double height) {
+    return Card(
+      //margin: EdgeInsets.all(10.0),
+      color: color,
+      elevation: 5.0,
+      child: Container(
+        width: width,
+        height: height,
       ),
     );
   }
