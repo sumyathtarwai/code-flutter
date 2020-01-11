093aa9f374c80d9990d6beadc3b3094f56d1eb3c
sumyathtarwai <hsumyathtarwai@gmail.com>
2020-01-11 12:01:13 +0630

    added my position

--- flutter/ui_building/lib/main.dart
+++ flutter/ui_building/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ui_building/ui/my_container.dart';
+import 'package:ui_building/ui/my_position.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.orangeAccent),
-      home: MyContainer(),
+      home: MyPosition(),
     );
   }
 }
