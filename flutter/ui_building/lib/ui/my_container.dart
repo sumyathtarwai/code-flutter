7c21ec93b7c2611c41df03646de38ebee19e51f2
sumyathtarwai <hsumyathtarwai@gmail.com>
2020-01-14 23:41:58 +0630

    xcode config

--- flutter/ui_building/lib/ui/my_container.dart
+++ flutter/ui_building/lib/ui/my_container.dart
@@ -3,14 +3,6 @@ import 'package:flutter/material.dart';
 class MyContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var borderRadius = BorderRadius.only(
-      topLeft: Radius.circular(
-        100.0,
-      ),
-      bottomRight: Radius.circular(
-        100.0,
-      ),
-    );
     return Scaffold(
       appBar: AppBar(),
       body: Container(
