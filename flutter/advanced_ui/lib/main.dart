1584f127e9e2df2ff4ef946c48319cdae41bc0b0
sumyathtarwai <hsumyathtarwai@gmail.com>
2020-01-18 12:02:50 +0630

    added decoration

---
+++ flutter/advanced_ui/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:advanced_ui/ui/my_decoration.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(
+        home: MyDecoration(),
+      ),
+    );
+  }
+}
