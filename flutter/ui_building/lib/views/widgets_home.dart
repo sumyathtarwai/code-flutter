75f8d46a3cea8733bd6ca641a685914c1a8b674d
sumyathtarwai <hsumyathtarwai@gmail.com>
2020-01-25 21:27:45 +0630

    change  to ListView.builder

--- flutter/ui_building/lib/views/widgets_home.dart
+++ flutter/ui_building/lib/views/widgets_home.dart
@@ -1,125 +1,50 @@
 import 'package:flutter/material.dart';
 
 class WidgetsHome extends StatelessWidget {
-  const WidgetsHome({Key key}) : super(key: key);
-
+  final routes = [
+    {'title': 'Buttons လေးပါနော်', 'routeName': '/button'},
+    {'title': 'Alert Box ကဒီမှာ', 'routeName': '/alert'},
+    {'title': 'Radio Buttons ကဘယ်လို?', 'routeName': '/radio'},
+    {'title': 'Checkbox ကတော့', 'routeName': '/checkbox'},
+    {'title': 'Input အမျိုးမျိုး', 'routeName': '/input'},
+    {'title': 'App Bar ဆိုတာ', 'routeName': '/appbar'},
+    {'title': 'Wallet Design Demo', 'routeName': '/wallet'},
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('❤️ Widgets'),
       ),
-      backgroundColor: Colors.indigo.shade100,
       body: Container(
         margin: EdgeInsets.all(16.0),
-        child: ListView(
-          padding: EdgeInsets.all(8.0),
-          children: <Widget>[
-            Card(
-              color: Colors.indigo.shade200,
-              child: FlatButton(
-                child: Text(
-                  "Buttons လေးပါနော်",
-                  style: TextStyle(
-                    fontFamily: "MyanmarNayone",
-                    fontSize: 20.0,
-                  ),
-                ),
-                onPressed: () => Navigator.of(context).pushNamed(
-                  '/button',
-                ),
-              ),
-            ),
-            Card(
-              color: Colors.indigo.shade200,
-              child: FlatButton(
-                child: Text(
-                  "Alert Box ကဒီမှာ",
-                  style: TextStyle(
-                    fontFamily: "MyanmarNayone",
-                    fontSize: 20.0,
-                  ),
-                ),
-                onPressed: () => Navigator.of(context).pushNamed(
-                  '/alert',
-                ),
-              ),
-            ),
-            Card(
-              color: Colors.indigo.shade200,
-              child: FlatButton(
-                child: Text(
-                  "Radio Buttons ကဘယ်လို?",
-                  style: TextStyle(
-                    fontFamily: "MyanmarNayone",
-                    fontSize: 20.0,
-                  ),
-                ),
-                onPressed: () => Navigator.of(context).pushNamed(
-                  '/radio',
-                ),
-              ),
-            ),
-            Card(
-              color: Colors.indigo.shade200,
-              child: FlatButton(
-                child: Text(
-                  "Checkbox ကတော့",
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontFamily: "MyanmarNayone",
-                  ),
-                ),
-                onPressed: () => Navigator.of(context).pushNamed(
-                  '/checkbox',
-                ),
-              ),
-            ),
-            Card(
-              color: Colors.indigo.shade200,
-              child: FlatButton(
-                child: Text(
-                  "App Bar ဆိုတာ",
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontFamily: "MyanmarNayone",
-                  ),
-                ),
-                onPressed: () => Navigator.of(context).pushNamed(
-                  '/buttons',
-                ),
-              ),
-            ),
-            Card(
-              color: Colors.indigo.shade200,
-              child: FlatButton(
-                child: Text(
-                  "Input အမျိုးမျိုး",
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontFamily: "MyanmarNayone",
-                  ),
-                ),
-                onPressed: () => Navigator.of(context).pushNamed(
-                  '/input',
-                ),
-              ),
-            ),
-            Card(
-              color: Colors.indigo.shade200,
-              child: FlatButton(
-                child: Text(
-                  "Wallet Design Demo",
-                  style: TextStyle(
-                    fontSize: 20.0,
-                  ),
-                ),
-                onPressed: () => Navigator.of(context).pushNamed(
-                  '/wallet',
-                ),
-              ),
-            ),
-          ],
+        child: ListView.builder(
+          itemBuilder: (context, i) {
+            return buildRoute(
+              context: context,
+              title: routes[i]['title'],
+              routeName: routes[i]['routeName'],
+            );
+          },
+          itemCount: routes.length,
+        ),
+      ),
+    );
+  }
+
+  Card buildRoute({BuildContext context, String title, String routeName}) {
+    return Card(
+      color: Colors.indigo.shade200,
+      child: FlatButton(
+        child: Text(
+          title,
+          style: TextStyle(
+            fontFamily: 'MyanmarNayone',
+            fontSize: 20.0,
+          ),
+        ),
+        onPressed: () => Navigator.of(context).pushNamed(
+          routeName,
         ),
       ),
     );
