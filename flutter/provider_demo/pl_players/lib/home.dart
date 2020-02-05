074a4b2117f0b883e71d1a6d87e9bd5cb844cad4
sumyathtarwai <hsumyathtarwai@gmail.com>
2020-02-05 20:47:38 +0630

    added Form and model

--- flutter/provider_demo/pl_players/lib/home.dart
+++ flutter/provider_demo/pl_players/lib/home.dart
@@ -1,20 +1,67 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key key}) : super(key: key);
+  final GlobalKey<FormState> _formKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Image.asset(
-          'assets/pl.png',
-          color: Colors.white,
+        appBar: AppBar(
+          leading: Image.asset(
+            'assets/pl.png',
+            color: Colors.white,
+          ),
+          titleSpacing: 30.0,
+          title: Text('Premier League'),
         ),
-        titleSpacing: 30.0,
-        title: Text('Premier League'),
-      ),
-      body: Container(),
-    );
+        body: Column(
+          children: <Widget>[
+            Form(
+              key: _formKey,
+              child: Container(
+                margin: EdgeInsets.all(20.0),
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: InputDecoration(
+                          labelText: 'Player Name',
+                          hintText: 'Virgil van Dijk'),
+                      validator: (val) =>
+                          val.isEmpty ? 'Player Name is required' : null,
+                      onSaved: (val) => '',
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          labelText: 'Club', hintText: 'Liverpool'),
+                      validator: (val) =>
+                          val.isEmpty ? 'Club Name is required' : null,
+                      onSaved: (val) => '',
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          labelText: 'Position', hintText: 'Defender'),
+                      validator: (val) =>
+                          val.isEmpty ? 'Position is required' : null,
+                      onSaved: (val) => '',
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    RaisedButton(
+                      onPressed: () => '',
+                      child: Text('Save'),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            ListView.builder(
+              itemCount: 1,
+              itemBuilder: (context, index) {
+                return;
+              },
+            ),
+          ],
+        ));
   }
 }
---
+++ flutter/provider_demo/pl_players/lib/model/player.dart
@@ -0,0 +1,7 @@
+class Player {
+  String playerName;
+  String clubName;
+  String position;
+
+  Player({this.playerName, this.clubName, this.position});
+}
