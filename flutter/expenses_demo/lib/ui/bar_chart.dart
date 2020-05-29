82e36cea4d1a4023007f97a2cf9d09036f581753
sumyathtarwai <hsumyathtarwai@gmail.com>
2020-05-30 01:19:02 +0630

    added chart

--- flutter/expenses_demo/lib/ui/bar_chart.dart
+++ flutter/expenses_demo/lib/ui/bar_chart.dart
@@ -7,7 +7,7 @@ class BarChart extends StatelessWidget {
 
   BarChart(this._transaction);
 
-  List<Map<String, Object>> get weeklySpend {
+  List<Map<String, Object>> get weeklySpendData {
     return List.generate(
       7,
       (index) {
@@ -32,16 +32,103 @@ class BarChart extends StatelessWidget {
     );
   }
 
+  double get weeklySpendTotal {
+    return weeklySpendData.fold(
+      0.0,
+      (prev, element) => prev + element['amount'],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 8,
       margin: EdgeInsets.all(10),
-      child: Row(
-        children: weeklySpend.map((e) {
-          return Text('${e['day']}: ${e['amount']}');
-        }).toList(),
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: Text(
+              'THIS WEEK',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: weeklySpendData.map((e) {
+                return Flexible(
+                  fit: FlexFit.tight,
+                  child: Chart(
+                    label: e['day'],
+                    amount: e['amount'],
+                    spendRange: weeklySpendTotal == 0.0
+                        ? 0.0
+                        : (e['amount'] as double) / weeklySpendTotal,
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class Chart extends StatelessWidget {
+  final String label;
+  final double amount;
+  final double spendRange;
+
+  const Chart({this.label, this.amount, this.spendRange});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: <Widget>[
+        FittedBox(
+          child: Text('\$ ${amount.toStringAsFixed(0)}'),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        Container(
+          height: 100,
+          width: 20,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: spendRange,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).accentColor,
+                    boxShadow: [
+                      BoxShadow(color: Colors.black12, offset: Offset(1, 2)),
+                    ],
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Text('$label'),
+      ],
     );
   }
 }
