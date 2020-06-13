d1c4a679e724750c1c2c7149a686eb66753987ea
sumyathtarwai <hsumyathtarwai@gmail.com>
2020-06-13 22:06:20 +0630

    init

---
+++ flutter/bloc/bloc_api_demo/README.md
@@ -0,0 +1,8 @@
+# bloc_api_demo
+
+Implement BLoc design.
+
+```
+dependencies:
+  flutter_bloc: ^4.0.0
+```
---
+++ flutter/bloc/bloc_api_demo/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BLoc Demo with API',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Container(),
+      ),
+    );
+  }
+}
