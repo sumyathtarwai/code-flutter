ac7eb8b2baba90766bde7c8c9fc0f0b0e4975340
sumyathtarwai <hsumyathtarwai@gmail.com>
2020-07-02 15:36:10 +0630

    update ui

--- flutter/recipe_demo/lib/ui/categories_item.dart
+++ flutter/recipe_demo/lib/ui/categories_item.dart
@@ -4,9 +4,10 @@ import '../const.dart';
 class CategoryItem extends StatelessWidget {
   final String id;
   final String title;
+  final String imageUrl;
   final Color color;
 
-  const CategoryItem({Key key, this.id, this.title, this.color})
+  const CategoryItem({Key key, this.id, this.title, this.imageUrl, this.color})
       : super(key: key);
 
   void _recipePagePath(BuildContext context) {
@@ -28,21 +29,31 @@ class CategoryItem extends StatelessWidget {
       child: Container(
         child: Center(
           child: FittedBox(
-            child: Text(
-              title,
-              style: Theme.of(context).textTheme.headline6,
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                title,
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    color.withOpacity(0.5),
+                    color.withOpacity(0.8),
+                    color,
+                  ],
+                  begin: Alignment.bottomRight,
+                  end: Alignment.topRight,
+                ),
+                borderRadius: BorderRadius.circular(15),
+              ),
             ),
           ),
         ),
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              color.withOpacity(0.5),
-              color.withOpacity(0.8),
-              color,
-            ],
-            begin: Alignment.bottomRight,
-            end: Alignment.topRight,
+          image: DecorationImage(
+            image: NetworkImage(imageUrl),
+            fit: BoxFit.cover,
           ),
           borderRadius: BorderRadius.circular(15),
         ),
